75956383a7690d3f4aadb78fe353aa9d5047145b
Suleman Daud <suleman.sdj@gmail.com>
2018-02-01 15:28:14 +0500

    Loading off all pages

--- login1.py
+++ login1.py
@@ -29,7 +29,7 @@ class LoginTests (unittest.TestCase):
         # self.driver.find_element_by_link_text('Employees').click()
 
     def test_employee_link(self):
-        employee = self.driver.find_element_by_id('emp-main-menu')
+        employee = self.driver.find_element_by_link_text('Employees')
         employee.click()
 
         assert "Your most important asset" in self.driver.page_source
@@ -59,6 +59,77 @@ class LoginTests (unittest.TestCase):
 
         assert  "Manage the powered ones" in self.driver.page_source
         self.driver.back()
+
+    def test_setting_bnd_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_xpath("//a[@href='/settings/branding-and-design']").click()
+        assert "Branding and Design" in self.driver.page_source
+
+    def test_setting_pnp_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_xpath("//a[@href='/settings/photo-id-printing']").click()
+        assert "Photo and ID Printing" in self.driver.page_source
+
+    def test_setting_pnp_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_xpath("//a[@href='/settings/visitor-fields']").click()
+        assert "Visitor Fields" in self.driver.page_source
+
+    def test_setting_agree_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_link_text('Visitor Agreement').click()
+        assert "Agreement Settings" in self.driver.page_source
+
+    def test_setting_vissetings_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_xpath("//a[@href='/settings/visitor-settings']").click()
+        assert "Visitor Settings" in self.driver.page_source
+
+    def test_setting_notificationsettings_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_xpath("//a[@href='/settings/notification-settings']").click()
+        assert "Notification Settings" in self.driver.page_source
+
+    def test_setting_empsettings_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_xpath("//a[@href='/settings/employee']").click()
+        assert "Employee Settings" in self.driver.page_source
+
+    def test_setting_ipadnlocations_link(self):
+        self.driver.find_element_by_xpath("//a[@href='/settings']").click()
+        self.driver.find_element_by_xpath("//a[@href='/settings/ipads-locations']").click()
+        assert "iPads and Locations" in self.driver.page_source
+
+    def test_account_companyname_link(self):
+            self.driver.find_element_by_link_text('Account').click()
+            self.driver.find_element_by_xpath("//a[@href='/billing/company-info']").click()
+
+            #self.driver.find_element_by_xpath("//a[@href='/settings/ipads-locations']").click()
+            assert "Company Info" in self.driver.page_source
+
+    def test_account_planpricing_link(self):
+        self.driver.find_element_by_link_text('Account').click()
+        self.driver.find_element_by_xpath("//a[@href='/billing/plans-and-pricing']").click()
+
+        # self.driver.find_element_by_xpath("//a[@href='/settings/ipads-locations']").click()
+        assert "Simple Pricing" in self.driver.page_source
+
+    def test_account_smsbilling_link(self):
+        self.driver.find_element_by_link_text('Account').click()
+        self.driver.find_element_by_link_text('Plans and Pricing').click()
+
+        # self.driver.find_element_by_xpath("//a[@href='/settings/ipads-locations']").click()
+        assert "SMS Billing Transactions" in self.driver.page_source
+
+    def test_account_planbilling_link(self):
+        self.driver.find_element_by_link_text('Account').click()
+        self.driver.find_element_by_link_text('Plans and Pricing').click()
+
+        # self.driver.find_element_by_xpath("//a[@href='/settings/ipads-locations']").click()
+        assert "Billing Transactions" in self.driver.page_source
+
+
+
 
     #def test_billing_link(self):
     #    self.driver.find_element_by_partial_link_text('Account').click()
